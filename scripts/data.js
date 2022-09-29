ee7385c64366624e798ed363297391bfe0080327
olexii ponomarenko <deka.pon.y@gmail.com>
2022-09-29 20:05:34 +0300

    almost done
    almost done

---
+++ scripts/data.js
@@ -0,0 +1,103 @@
+import { v4 as uuidv4 } from 'https://jspm.dev/uuid';
+
+const myDataStorage = { items:[]};
+const myDataStorageArchived = {items:[]};
+
+export const readLS = () => {
+	try {
+		const items1 = (JSON.parse(localStorage.getItem("data"))).items;
+		const items2 = (JSON.parse(localStorage.getItem("archivedData"))).items;
+		return [items1, items2];
+	}catch(e){
+		console.log(e);
+		return [[],[]];
+	}
+}
+
+export const reset = (myDataStorage, myDataStorageArchived) => {
+	console.log("reset")
+	const defaultData = createDefaultData();
+	myDataStorage.items = defaultData[0].items;
+	myDataStorageArchived.items  = defaultData[1].items;
+	//writeLS(myDataStorage, myDataStorageArchived);
+	//readLS();
+}
+
+export const writeLS = (myDataStorage, myDataStorageArchived) => {
+	console.log("writeLS");
+	//console.log(myDataStorage);
+	//console.log(myDataStorageArchived);
+	try {
+		localStorage.setItem("data", JSON.stringify(myDataStorage));
+		localStorage.setItem("archivedData", JSON.stringify(myDataStorageArchived));
+	}catch(e){
+		// якщо перевищено ліміт
+		 if (e.name == "QUOTA_EXCEEDED_ERR") alert('QUOTA_EXCEEDED_ERR:  localStorage перевищено допустимий ліміт');
+		else alert(e);
+	}
+}
+
+export const createDefaultData = () => {
+	console.log("createDefaultData")
+	//const randDate = () =>  {
+	//	const d1 = new Date();
+	//	//console.log(`${d1.getDate()}.${d1.getMonth()}.${d1.getFullYear()}`)
+	//	return d1;
+	//}
+
+	const tempMyDataStorage = {
+		items: [
+			{	id: uuidv4(),
+				name: "Shopping list",
+				Created: new Date(),
+				category: "Task",
+				content: "Tomatoes, bread",
+				dates: "5-55-5555",
+			},
+			{	id: uuidv4(),
+				name: "The theory of evolution",
+				Created: new Date(),
+				category: "Random Thought",
+				content: "The evolution lorum",
+				dates: "",
+			},
+			{	id: uuidv4(),
+				name: "New feature",
+				Created: new Date(),
+				category: "Idea",
+				content: "Implement new feature",
+				dates: "",
+			},
+
+		]};
+	const tempMyDataStorageArchived = {
+		items: [
+			{	id: uuidv4(),
+				name: "Shopping list a",
+				Created: new Date(),
+				category: "Task",
+				content: "Tomatoes, bread, tea",
+				dates: "",
+			},
+			{	id: uuidv4(),
+				name: "The lol a",
+				Created: new Date(),
+				category: "Random Thought",
+				content: "The lol kek",
+				dates: "",
+			},
+			{	id: uuidv4(),
+				name: "New text a",
+				Created: new Date(),
+				category: "Idea",
+				content: "text ",
+				dates: "",
+			},
+
+	]};
+		return [tempMyDataStorage, tempMyDataStorageArchived];
+}
+	
+
+
+
---
+++ scripts/main.js
@@ -0,0 +1,199 @@
+	import { v4 as uuidv4 } from 'https://jspm.dev/uuid';
+	import {openForm, closeForm, submitBtn} from "/scripts/forms.js"
+	import {readLS, reset, writeLS} from "/scripts/data.js"
+	import {refreshTable, setItemListeneres} from "/scripts/tables.js"
+	//console.log(uuidv4()); // ⇨ '1b9d6bcd-bbfd-4b2d-9b5d-ab8dfbbd4bed'	
+
+	console.log("start");
+	const myDataStorage = { items:[]};
+	const myDataStorageArchived = {items:[]};
+	[myDataStorage.items, myDataStorageArchived.items] = readLS();
+	
+	const addItemform = document.querySelector(".form-container-add");
+	const editItemform = document.querySelector(".form-container-edit");
+
+	const createCategoryes = () => {
+	const myCategoryes = {rand: "Random Thought", task:"Task", idea: "Idea"};
+	const categoryesSel = [];
+	Object.keys(myCategoryes).forEach((item, index)=>{
+			categoryesSel[index] = `<option value="${myCategoryes[item]}">${myCategoryes[item]}</option>`;
+	});
+	addItemform.querySelector(`#itemCategory`).innerHTML=categoryesSel;
+	editItemform.querySelector(`#itemCategory`).innerHTML=categoryesSel;
+	return myCategoryes;
+	}
+	const myCategoryes = createCategoryes();
+	//console.log(myCategoryes);
+
+
+	const resetData = () => {
+		console.log("reset strt")
+		reset(myDataStorage, myDataStorageArchived);
+		writeLS(myDataStorage, myDataStorageArchived);
+		[myDataStorage.items, myDataStorageArchived.items] = readLS();
+		refreshTable(myDataStorage, myDataStorageArchived);
+		setEvents();
+		console.log("reset end")
+	}
+
+//console.log(addItemform);
+
+	//const dataTable = document.querySelector(".main-table");
+	//const archDataTable = document.querySelector(".arch-table");
+
+	const submitform = (event) => {
+		console.log("submitform");
+		//const newItem = {id: uuidv4(), ...submitBtn(event)};
+		const newItem = submitBtn(event);
+		newItem.id =uuidv4();
+		console.log(event.path[0]);
+		const form = event.path[0];
+		closeForm(form);
+		myDataStorage.items.push(newItem);
+		//console.log(myDataStorage);
+		writeLS(myDataStorage, myDataStorageArchived);
+		[myDataStorage.items, myDataStorageArchived.items] = readLS();
+		refreshTable(myDataStorage, myDataStorageArchived);
+		setEvents();
+	}
+
+	const submitEditForm = (event) => {
+		console.log("submitEditForm");
+		//const newItem = {id: uuidv4(), ...submitBtn(event)};
+		const EditedItem = submitBtn(event);
+		//newItem.id =uuidv4();
+		console.log(EditedItem);
+		const form = event.path[0];
+		closeForm(form);
+		
+		myDataStorage.items.find((item, index)=> {
+			if(item.id === form.customID) {
+				console.log("finded!");
+				console.log(index)
+				console.log(item)
+				myDataStorage.items[index] = {...item, ...EditedItem};
+				//item.dates = EditedItem.dates;
+				console.log(item)
+				return true;
+			} 
+		});
+		console.log(myDataStorage);
+		//alert("f")
+		writeLS(myDataStorage, myDataStorageArchived);
+		[myDataStorage.items, myDataStorageArchived.items] = readLS();
+		refreshTable(myDataStorage, myDataStorageArchived);
+		setEvents();
+	}
+	
+	document.addEventListener("load", refreshTable(myDataStorage, myDataStorageArchived));
+
+	const delF = (e) => {
+		console.log("delF"); 
+		//console.log(e); 
+		//console.log(e.path[3]);
+		//console.log(e.path[3].customID);
+		//console.log(myDataStorage)
+
+		myDataStorage.items.find(function(item, index) {
+			if (item.id === e.path[3].customID) {
+				myDataStorage.items.splice(index,1);
+				console.log(myDataStorage);
+				localStorage.setItem("data", JSON.stringify(myDataStorage));
+				refreshTable(myDataStorage, myDataStorageArchived);
+				setEvents();
+				return [];
+			};	
+			
+		})
+	}
+
+	const archF = (e) => {
+		console.log("archF"); 
+		//console.log(e); 
+		//console.log(e.path[3]);
+		//console.log(e.path[3].customID);
+		//console.log(myDataStorage)
+
+		myDataStorage.items.find(function(item, index) {
+			if (item.id === e.path[3].customID) {
+				const deletedItem = myDataStorage.items.splice(index,1);
+				console.log(deletedItem);
+				//myDataStorageArchived
+				myDataStorageArchived.items.push(deletedItem[0])
+				console.log(myDataStorage);
+				localStorage.setItem("data", JSON.stringify(myDataStorage));
+				localStorage.setItem("archivedData", JSON.stringify(myDataStorageArchived));
+				refreshTable(myDataStorage, myDataStorageArchived);
+				setEvents();
+				return [];
+			};	
+			
+		})
+
+		
+	}
+
+	const delFArch = (e) => {
+		console.log("delFArch"); 
+		//console.log(e); 
+		//console.log(e.path[3]);
+		//console.log(e.path[3].customID);
+		console.log(myDataStorageArchived);
+
+		myDataStorageArchived.items.find(function(item, index) {
+			if (item.id === e.path[3].customID) {
+				myDataStorageArchived.items.splice(index,1);
+				console.log(myDataStorageArchived);
+				localStorage.setItem("archivedData", JSON.stringify(myDataStorageArchived));
+				refreshTable(myDataStorage, myDataStorageArchived);
+				setEvents();
+				return [];
+			};	
+			
+		})
+
+
+	}
+
+	const editF = (e) => {
+		console.log("editBtnF");
+		console.log(e.path[3].customID);
+		const item = myDataStorage.items.find((item ) => item.id === e.path[3].customID );
+		openForm(editItemform, item);
+	}
+
+	const setEvents = () => {
+		console.log("setEvents");
+		const delBtnQSel = ".main-table>tr>th>div>.delete-btn"; // del btn 
+		const editBtnQSel = ".main-table>tr>th>div>.edit-btn"; // edit btn 
+		const archBtnQSel = ".main-table>tr>th>div>.archive-btn"; // edit btn 
+		const delBtnQSelArchive = ".arch-table>tr>th>div>.delete-btn"; // del btn archived table
+		setItemListeneres(delBtnQSel, delF);
+		setItemListeneres(editBtnQSel, editF);
+		setItemListeneres(archBtnQSel, archF);
+		setItemListeneres(delBtnQSelArchive, delFArch);
+	}
+
+	setEvents();
+
+	document.querySelector(".addBtn").addEventListener("click", ()=> openForm(addItemform));
+	addItemform.querySelector(".cancel").addEventListener("click", ()=> closeForm(addItemform));
+	editItemform.querySelector(".cancel").addEventListener("click", ()=> closeForm(editItemform));
+
+	document.querySelector(".form-background").addEventListener("click", ()=>  closeForm(addItemform));
+	document.querySelector(".form-background").addEventListener("click", ()=> closeForm(editItemform));
+
+	document.querySelector(".form-container-add").addEventListener("submit", (event) => {submitform(event)});
+	document.querySelector(".form-container-edit").addEventListener("submit", (event) => {submitEditForm(event)});
+
+	document.querySelector(".resetBtn").addEventListener("click", resetData);
+
+	document.onkeydown = function(evt) {
+		if (evt.key === "Escape") {
+			closeForm(addItemform);
+			closeForm(editItemform);
+		};
+		};
+
+
+	
